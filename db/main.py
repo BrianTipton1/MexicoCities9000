a369de315adca71a29021206a1433b9fb52e74ff
Brian Tipton <briantiptondev@gmail.com>
2024-01-27 23:14:59 -0600

    data gen

--- db/main.py
+++ db/main.py
@@ -1,19 +1,34 @@
+from bson import Binary
 import openrouteservice
+from uuid import uu
 import msgpack
 import json
 from itertools import combinations, islice
-from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
 from pandas import read_csv
 import redis
 from openrouteservice import convert
-
-def get_location_key(index):
-    return f'{index}:location'
-
-def get_route_key(point_one, point_two):
-    return f'{point_one}:route:{point_two}'
+from pymongo import MongoClient
 
 CLIENT = openrouteservice.Client(base_url="http://172.21.1.3:8080/ors")
+
+def get_mongo_collection():
+    client = MongoClient('mongodb://root:example@127.0.0.1:27017/')
+    db = client.routes
+    return db['points']
+
+def store_messagepack_data(collection, data):
+    serialized_data = msgpack.packb(data, use_bin_type=True)
+    
+    collection.insert_one({'0': serialized_data})
+
+def get_location_key(index):
+    return f"{index}:location"
+
+
+def get_route_key(point_one, point_two):
+    return f"{point_one}:route:{point_two}"
+
 
 def get_mexico_super_chargers() -> list:
     df = read_csv("data/SuperchargeLocations.csv")
@@ -25,8 +40,10 @@ def get_mexico_super_chargers() -> list:
     return filtered_df[["City", "GPS", "IsSuperCharger"]].to_dict(orient="records")
 
 
-def get_distance(to_coords, from_coords):
-    routes = CLIENT.directions((to_coords, from_coords))
+def get_distance(point_one, point_two):
+    routes = CLIENT.directions(
+        (point_one, point_two), units="km", preference="shortest", instructions=False
+    )
     return routes
 
 
@@ -42,29 +59,30 @@ def get_all_mexican_cities():
             for item in data
         ]
 
-def insert_distance_data(key, distance):
-    redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0)
-    redis_client.set(key, distance)
+
+def insert_distance_data(key, results):
+    redis_client = redis.Redis(host="127.0.0.1", port=6379, db=0)
+    collection = get_mongo_collection()
+    store_messagepack_data(collection, results['points'])
+    redis_client.set(key, results['distance'])
+
 
 def transform_response(raw_response):
     try:
-        route = raw_response['routes'][0]
-        total_distance = route['summary']['distance']
-        # total_steps = sum(len(segment['steps']) for segment in route['segments'])
-        return total_distance
-        # points = convert.decode_polyline(route['geometry'])
-
-        # return {
-            # 'distance': total_distance,
-            # 'steps': total_steps,
-            # 'points': points
-        # }
+        route = raw_response["routes"][0]
+        total_distance = route["summary"]["distance"]
+        points = convert.decode_polyline(route["geometry"])["coordinates"]
+        return {"distance": total_distance, "points": points}
     except:
-        print('SHIT THE BED')
+        print("SHIT THE BED")
         return 1
 
+
 def process_combinations_chunk(combos):
-    for (point_one_index, point_one_location), (point_two_index, point_two_location) in combos:
+    for (point_one_index, point_one_location), (
+        point_two_index,
+        point_two_location,
+    ) in combos:
         lat1, long1 = point_one_location["GPS"]
         lat2, long2 = point_two_location["GPS"]
         distance = transform_response(get_distance((long1, lat1), (long2, lat2)))
@@ -76,26 +94,22 @@ if __name__ == "__main__":
     cities = get_all_mexican_cities()
     relevant_points = list(enumerate(super_chargers + cities))
 
-    redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0)
+    redis_client = redis.Redis(host="127.0.0.1", port=6379, db=0)
     redis_client.flushall()
 
-    print('')
+    print("")
     for index, data in enumerate(relevant_points):
         packed_data = msgpack.packb(data)
         redis_client.set(get_location_key(index), packed_data)
 
     combo_iterator = combinations(relevant_points, 2)
 
-    num_processes = 12
-    pool = Pool(num_processes)
-
+    num_threads = 12
     chunk_size = 1000
 
-    while True:
-        chunk = list(islice(combo_iterator, chunk_size))
-        if not chunk:
-            break
-        pool.apply_async(process_combinations_chunk, args=[chunk])
-
-    pool.close()
-    pool.join()
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        while True:
+            chunk = list(islice(combo_iterator, chunk_size))
+            if not chunk:
+                break
+            executor.submit(process_combinations_chunk, chunk)
