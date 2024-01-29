15473601e7bf32e1404047d3f44626d7bc9c417c
Brian Tipton <briantiptondev@gmail.com>
2024-01-28 20:55:42 -0600

    matrix time

---
+++ db/extract_data.py
@@ -0,0 +1,54 @@
+import redis
+import msgpack
+import requests
+import json
+
+ORS_BASE_URL = "http://172.21.1.3:8080/ors/v2/matrix/driving-car"
+
+def get_matrix(locations):
+    url = ORS_BASE_URL
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+    }
+    payload = {
+        "locations": locations,
+        "metrics": ["distance"],
+        "resolve_locations": "false",
+        "units": "km",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    return response.json()
+
+def extract_data(redis_client, key):
+    packed_data = redis_client.get(f'{key}')
+    if packed_data is not None:
+        return msgpack.unpackb(packed_data)
+    else:
+        return None
+
+def main():
+    redis_client = redis.Redis(
+        host="172.21.1.9", port=6379, db=0, decode_responses=False
+    )
+
+    key1 = '3623:location'
+    key2 = '3664:location'
+
+    data1 = extract_data(redis_client, key1)
+    data2 = extract_data(redis_client, key2)
+
+    # Assuming data format is [index, data_dict]
+    start_gps = data1[1]['GPS']
+    end_gps = data2[1]['GPS']
+
+    request_url = f"http://172.21.1.3:8080/ors/v2/directions/driving-car?start={start_gps[0]},{start_gps[1]}&end={end_gps[0]},{end_gps[1]}"
+
+    
+    # print(request_url)
+    print(json.dumps(get_matrix([start_gps, end_gps])))
+    print('')
+
+if __name__ == "__main__":
+    main()
--- db/main.py
+++ db/main.py
@@ -1,35 +1,14 @@
-from bson import Binary
-import openrouteservice
-from uuid import uuid4
+import itertools
+import aiohttp
 import msgpack
+import redis.asyncio as redis
 import json
-from itertools import combinations, islice
-from concurrent.futures import ThreadPoolExecutor
+import asyncio
 from pandas import read_csv
-import redis
-from openrouteservice import convert
-from pymongo import MongoClient
+import pandas as pd
+import numpy as np
 
-CLIENT = openrouteservice.Client(base_url="http://172.21.1.3:8080/ors")
-
-
-def get_mongo_collection():
-    client = MongoClient("mongodb://root:example@172.21.1.9:27017/")
-    db = client.routes
-    return db["points"]
-
-
-def store_messagepack_data(collection, data, key):
-    serialized_data = msgpack.packb(data, use_bin_type=True)
-    collection.insert_one({key: serialized_data})
-
-
-def get_location_key(index):
-    return f"{index}:location"
-
-
-def get_route_key(point_one, point_two):
-    return f"{point_one}:route:{point_two}"
+ORS_BASE_URL = "http://172.21.1.3:8080/ors/v2/matrix/driving-car"
 
 
 def get_mexico_super_chargers() -> list:
@@ -42,11 +21,13 @@ def get_mexico_super_chargers() -> list:
     return filtered_df[["City", "GPS", "IsSuperCharger"]].to_dict(orient="records")
 
 
-def get_distance(point_one, point_two):
-    routes = CLIENT.directions(
-        (point_one, point_two), units="km", preference="shortest", instructions=False
-    )
-    return routes
+def transform_response(raw_response):
+    try:
+        distances = raw_response["distances"]
+        return distances
+    except KeyError:
+        print("Error in the response data")
+        return False
 
 
 def get_all_mexican_cities():
@@ -62,56 +43,126 @@ def get_all_mexican_cities():
         ]
 
 
-def insert_distance_data(key, results):
-    redis_client = redis.Redis(host="172.21.1.9", port=6379, db=0)
-    collection = get_mongo_collection()
-    store_messagepack_data(collection, results["points"], key=key)
-    redis_client.set(key, results["distance"])
+def get_location_key(index):
+    return f"{index}:location"
 
 
-def transform_response(raw_response):
-    try:
-        route = raw_response["routes"][0]
-        total_distance = route["summary"]["distance"]
-        points = convert.decode_polyline(route["geometry"])["coordinates"]
-        return {"distance": total_distance, "points": points}
-    except:
-        print("SHIT THE BED")
-        return 1
+def get_route_key(point_one, point_two):
+    return f"{point_one}:route:{point_two}"
 
 
-def process_combinations_chunk(combos):
-    for (point_one_index, point_one_location), (
-        point_two_index,
-        point_two_location,
-    ) in combos:
-        lat1, long1 = point_one_location["GPS"]
-        lat2, long2 = point_two_location["GPS"]
-        distance = transform_response(get_distance((long1, lat1), (long2, lat2)))
-        insert_distance_data(get_route_key(point_one_index, point_two_index), distance)
+def get_payload(locations):
+    return {
+        "locations": locations,
+        "metrics": ["distance"],
+        "resolve_locations": "false",
+        "units": "km",
+    }
+
+
+def get_headers():
+    return {
+        "Content-Type": "application/json; charset=utf-8",
+        "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+    }
+
+
+def find_value(matrix, target):
+    target_whole = int(target)
+    for row_index, row in enumerate(matrix):
+        for col_index, value in enumerate(row):
+            if int(value) == target_whole:
+                return row_index, col_index
+    return None
+
+
+def create_redis_key_matrix_map(lst):
+    return {f'{i}:route:{j}': (lst.index(i), lst.index(j)) for i in lst for j in lst}
+
+
+async def make_req(redis_client, session, pairs_chunk):
+    locations = list(
+        set(
+            itertools.chain.from_iterable(
+                [
+                    [(point1[0], point1[1]["GPS"]), (point2[0], point2[1]["GPS"])]
+                    for point1, point2 in pairs_chunk
+                ]
+            )
+        )
+    )
+    
+    mapping = create_redis_key_matrix_map(list(zip(*locations))[0])
+    payload = get_payload(list(zip(*locations))[1])
+    headers = get_headers()
+
+    async with session.post(ORS_BASE_URL, json=payload, headers=headers) as response:
+        if response.status == 200:
+            response_data = await response.json()
+            distances_matrix = transform_response(response_data)
+            if distances_matrix:
+                redis_pipeline = redis_client.pipeline()
+
+                for key, (matrix_row, matrix_column) in mapping.items():
+                    distance = distances_matrix[matrix_row][matrix_column]
+                    redis_pipeline.set(
+                        key,
+                        "NONE" if distance is None else float(distance),
+                    )
+
+                await redis_pipeline.execute()
+        else:
+            response_text = await response.text()
+            print(
+                f"Failed to get a successful response: {response.status} - {response_text}"
+            )
+
+
+async def main():
+    super_chargers = get_mexico_super_chargers()
+    cities = get_all_mexican_cities()
+    all_points = super_chargers + cities
+    relevant_points = [
+        (
+            index,
+            {
+                "City": point["City"],
+                "GPS": (point["GPS"][1], point["GPS"][0]),
+                "IsSuperCharger": point["IsSuperCharger"],
+            },
+        )
+        for index, point in enumerate(all_points)
+    ]
+
+    redis_client = redis.Redis(
+        host="172.21.1.9", port=6379, db=0, decode_responses=True
+    )
+
+    await redis_client.flushall()
+
+    for index, data in enumerate(relevant_points):
+        packed_data = msgpack.packb(data)
+        await redis_client.set(get_location_key(index), packed_data)
+
+    all_combinations = itertools.combinations(relevant_points, 2)
+    chunk_size = 100
+
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        while True:
+            pairs_chunk = list(itertools.islice(all_combinations, chunk_size))
+            if not pairs_chunk:
+                break
+            task = make_req(redis_client, session, pairs_chunk)
+            tasks.append(task)
+
+            if len(tasks) >= 10:
+                await asyncio.gather(*tasks)
+                tasks = []
+
+        if tasks:
+            await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    super_chargers = get_mexico_super_chargers()
-    cities = get_all_mexican_cities()
-    relevant_points = list(enumerate(super_chargers + cities))
-
-    redis_client = redis.Redis(host="172.21.1.9", port=6379, db=0)
-    # redis_client.flushall()
-
-    print("")
-    for index, data in enumerate(relevant_points):
-        packed_data = msgpack.packb(data)
-        redis_client.set(get_location_key(index), packed_data)
-
-    combo_iterator = combinations(relevant_points, 2)
-
-    num_threads = 12
-    chunk_size = 1000
-
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        while True:
-            chunk = list(islice(combo_iterator, chunk_size))
-            if not chunk:
-                break
-            executor.submit(process_combinations_chunk, chunk)
+    asyncio.run(main())
